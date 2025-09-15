39b7500c70cd2cdbd7afd5d07f7864bc72e07017
Matéo <marill.mateo@gmail.com>
2025-09-15 19:56:59 +0200

    fix

--- database/createDatabase.js
+++ database/createDatabase.js
@@ -11,6 +11,7 @@ async function createDatabase() {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
 
     const dbName = process.env.DB_NAME;
